0ba5117aaac874550ed205a35d95ac7ec0b28e19
Onur Vallor <onurvallor@gmail.com>
2025-12-15 18:08:34 -0500

    Added comments to explain what is happening

--- 10_5.cpp
+++ 10_5.cpp
@@ -4,11 +4,14 @@
 int main() {
 
   int i{5};
+  // Displays the type of the variable i.
   std::cout << "type of: " << typeid(i).name() << " = " << i << "\n";
 
   double d{5.5};
+  // Displays the type of the variable d.
   std::cout << "type of: " << typeid(d).name() << " = " << d << "\n";
 
+  // Displays the implicit conversion from into to double value.
   std::cout << typeid(i + d).name() << " with value of " << i + d << "\n";
 
   return 0;
