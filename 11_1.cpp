4c77c76f1972552cd4852d7184e67735e89dc913
Onur Vallor <onurvallor@gmail.com>
2025-12-16 09:05:03 -0500

    Added comments

--- 11_1.cpp
+++ 11_1.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
+// function overloads
 int add(int x, int y) { return x + y; }
-
 double add(double x, double y) { return x + y; }
 
+// Using template to be able to create a generic function for printing info (eg.
+// add with int, add with double).
 template <typename Func, typename X, typename Y>
 void printInfo(Func func, X x, Y y) {
   std::cout << "Result : " << func(x, y) << "\n";
@@ -11,6 +13,7 @@ void printInfo(Func func, X x, Y y) {
 
 int main() {
 
+  // calling function and passing fuction reference with the expected data.
   printInfo(static_cast<int (*)(int, int)>(add), 2, 3);
   printInfo(static_cast<double (*)(double, double)>(add), 2.5, 3.0);
 
