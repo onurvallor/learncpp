614f99294dbecb8800af3a1ad9f9d03f7dcd5432
Onur Vallor <onurvallor@gmail.com>
2025-12-29 15:22:28 -0500

    Structs and overloading operator<< for printing them

---
+++ 13_7.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ostream>
+
+struct Employee {
+  int id{};
+  int age{};
+  double wage{};
+};
+
+std::ostream &operator<<(std::ostream &out, const Employee &e) {
+  return out << e.id << " " << e.age << " " << e.wage << "\n";
+}
+
+int main() {
+
+  Employee joe{};
+
+  joe.age = 100;
+
+  std::cout << joe;
+
+  return 0;
+}
